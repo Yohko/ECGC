018f286a3f225d2227caedd4938a21391d1f51a4
Matthias Richter <matthias.h.richter@gmail.com>
2019-01-06 20:00:54 -0800

    Update startgc.cpp

--- startgc.cpp
+++ startgc.cpp
@@ -1,3 +1,5 @@
+// Licence: GNU General Public License version 2 (GPLv2)
+// For starting a SRI GC within EC-Lab using the "external application technique"
 #include <windows.h>
 #include <iostream>
 #include <unistd.h>
@@ -21,19 +23,51 @@ int main(int argc, char *argv[]){
 		i = atoi(argv[1]);
 	}else{
 		return 0;
+	}	
+
+	HINSTANCE hDLL = LoadLibraryW (L"user32.dll");
+	if (hDLL == NULL){
+		cout << "Failed to load user32.dll" << endl;
+		return 0;
 	}
+	typedef BOOL (WINAPI *BLOCKINPUT)(BOOL);
+	BLOCKINPUT pBlockInput;
+	pBlockInput = (BLOCKINPUT)GetProcAddress (hDLL, "BlockInput");
+    if(pBlockInput == NULL){
+		cout << "Failed to import BlockInput" << endl;
+		FreeLibrary(hDLL);
+		return 0;
+    }
 
 	cout << "GC will be started after timer ends." << endl;
-	
+
 	// wait for the given time
 	for(int j = 0;j<i;++j){
 		cout << "\rT minus " << i-j << " seconds ";
 		sleep(1); // sleep for 1 second
 	}
+
+	// block user input as this sometimes can interfere with starting the GC
+	// this needs Administrator privileges
+	BOOL res = pBlockInput(TRUE);
+	if(!res){
+		cout << "BlockInput failed" << endl;
+	}else{
+		cout << "BlockInput was successful." << endl;
+	}	
+	sleep(1);
+
 	// activate the GC program
-	HWND    hwndNotepad = FindWindowA(NULL,"Peaksimple - PeakSimple");
-	SetForegroundWindow(hwndNotepad);
+	HWND    hwndGC = FindWindowA(NULL,"Peaksimple - PeakSimple");
+	SetForegroundWindow(hwndGC);
+
 	// send "space" to start the GC
 	keybd_event(VkKeyScan(' '),0,0,0);
+
+	// wait a second and unblock user input
+	sleep(1);
+	pBlockInput(FALSE);
+
+	FreeLibrary(hDLL);
 	return 0;
 }
